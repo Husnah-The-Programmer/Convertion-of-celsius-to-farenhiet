0e5cbb861a858c47b8645813bb75310c84dcd11f
Husnah-The-Programmer <67584836+Husnah-The-Programmer@users.noreply.github.com>
2020-09-30 17:28:24 +0100

    Add files via upload

---
+++ Convertion of celsius to farenhiet.py
@@ -0,0 +1,5 @@
+#convertion of celcius to farenheit
+celsius=int(input("Enter the temperature in celsius:")) 
+farenheit=(celsius*1.8)+32
+print("%0.1f degree Celsius is equal to %0.1f degree Farenheit"%(celsius,farenheit))
+print("celsius is",farenheit)
